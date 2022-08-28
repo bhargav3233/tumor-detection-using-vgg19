8ea35dcea5eb0e15677ea264d00dba7b56b50c60
bhargav3233 <109935418+bhargav3233@users.noreply.github.com>
2022-08-28 13:27:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,76 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 21 10:24:17 2022
-
-@author: Bhargav
-"""
-
 import streamlit as st
-import tensorflow as tf
-import streamlit as st
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
-
-
-@st.cache(allow_output_mutation=True)
-
-def load_model():
-  model=tf.keras.models.load_model('D:/Tumor/tumor_vgg19.h5')
-  return model
-with st.spinner('Model is being loaded..'):
-  model=load_model()
-  
-st.write("""
-         # Brain Tumor Detection
-         """
-         )
-
-
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
-import cv2
-from PIL import Image, ImageOps
+from PIL import Image
 import numpy as np
-st.set_option('deprecation.showfileUploaderEncoding', False)
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 
+st.title('Brain Tumor Disease Detection')
 
+def main() :
+    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    if file_uploaded is not None :
+        image = Image.open(file_uploaded)
+        st.write("Uploaded Image.")
+        figure = plt.figure()
+        plt.imshow(image)
+        plt.axis('off')
+        st.pyplot(figure)
+        result, confidence = predict_class(image)
+        st.write('Prediction : {}'.format(result))
+        st.write('Confidence : {}%'.format(confidence))
 
-def upload_predict(upload_image, model):
-    size = (180,180)    
-    image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
-    image = np.asarray(image)
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img_resize = cv2.resize(img, dsize=(224, 224),interpolation=cv2.INTER_CUBIC)
-    
-    img_reshape = img_resize[np.newaxis,...]
+def predict_class(image) :
+    with st.spinner('Loading Model...'):
+        classifier_model = keras.models.load_model(r'D:/tumortype/brain_tumor_detector.h5', compile = False)
 
-    prediction = model.predict(img_reshape)
-    pred_class=decode_predictions(prediction,top=1)
-    
-    return pred_class
+    shape = ((224,224,3))
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])   
+    test_image = image.resize((224, 224))
+    test_image = keras.preprocessing.image.img_to_array(test_image)
+    test_image /= 255.0
+    test_image = np.expand_dims(test_image, axis = 0)
+    class_name = ['Meningioma_Tumor', 'Pituitary_Tumor', 'Glioma_Tumor', 'No_Tumor']
+    prediction = model.predict(test_image)
+    confidence = round(100 * (np.max(prediction[0])), 2)
+    final_pred = class_name[np.argmax(prediction)]
+    return final_pred, confidence
 
-          
-        
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = upload_predict(image, model)
-    label = tf.nn.softmax(prediction[0])
-    st.write(prediction)
-    st.write(label)
-    if label == 0:
-        st.write("This Person has a brain tumor")
-    else:
-        st.write("This person scan is healthy")
-   
-    
-   
-    
-   
-    
-"""img = image
-   #data = np.ndarray(shape=(224, 224) 
-   size = (224, 224)
-   image = ImageOps.fit(image, size, Image.ANTIALIAS)
-   
-   #image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-   #turn the image into a numpy array
-   image = np.asarray(image)
-   
-   img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-   
-   img_resize = cv2.resize(img, dsize=(224, 224),interpolation=cv2.INTER_CUBIC)
-   # Normalize the image
-   img = (img.astype(np.float32) / 127.0) - 1
-   
-   #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
-   
-   #img_reshape = img[np.newaxis,...]
-   # Load the image into the array
-   data[0] = img"""
+footer = """<style>
+a:link , a:visited{
+    color: white;
+    background-color: transparent;
+    text-decoration: None;
+}
+a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: None;
+}
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: transparent;
+    color: black;
+    text-align: center;
+}
+</style>
+<div class="footer">
+<p align="center"> <a href="https://www.linkedin.com/in/bhargav-reddy-8602b1221/">Developed with ❤ by Bhargav</a></p>
+</div>
+        """
+
+st.markdown(footer, unsafe_allow_html = True)
+
+if __name__ == '__main__' :
+    main()
